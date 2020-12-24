95f9fd5053e24a2bb6c45ef1fba0cefab66f1ff0
Hanif Aliffudin <68997219+hanifaliffudin@users.noreply.github.com>
2020-12-24 15:33:39 +0700

    Add files via upload

---
+++ portfolio/index.js
@@ -0,0 +1,11 @@
+// document.querySelector('#project').style.display = "none";
+
+function myWorks() {
+  document.querySelector('#about').style.display = "none";
+  document.querySelector('#project').style.display = "block";
+}
+
+function home() {
+  document.querySelector('#about').style.display = "block";
+  document.querySelector('#project').style.display = "none";
+}
